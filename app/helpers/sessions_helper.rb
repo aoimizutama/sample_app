b7c4c2105cd9bee592d872ba096bda7199df8338
aoimizutama <151143991+aoimizutama@users.noreply.github.com>
2024-09-09 12:32:58 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,7 +15,7 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # 現在ログイン中のユーザーを返す（いる場合）
+  # 記憶トークンのcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
       user = User.find_by(id: user_id)
@@ -29,6 +29,11 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
+
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
   end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
@@ -48,5 +53,10 @@ module SessionsHelper
     forget(current_user)
     reset_session
     @current_user = nil   # 安全のため
+  end
+
+  # アクセスしようとしたURLを保存する
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
